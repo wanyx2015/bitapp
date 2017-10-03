e2a361800b1b9c011acf082d4c96e07007dd0aa8
Wan <yuxi.wan@gmail.com>
2017-10-03 08:34:25 +0800

    add hbs partial support

--- server.js
+++ server.js
@@ -6,57 +6,66 @@ const coin = require('./coin/coin');
 
 var app = express();
 
-// set server rendering engine
+
+// add partial support for hbs
+hbs.registerPartials(__dirname + '/views/partials');
+
+// set server rendering engine to hbs
 app.set('view engine', 'hbs');
 
 // provide static html file access
 app.use(express.static(__dirname + './public'));
 
 app.use((req, res, next) => {
-
     var rightNow = new Date().toLocaleString();
     var user_agent = req.headers['user-agent'];
     console.log(rightNow, user_agent);
-    // console.log(JSON.stringify(req.headers, undefined, 4));
-    // console.log(req.headers['user-agent']);
     next();
 })
 
-
-
-
 app.get('/', (req, res) => {
-    console.log(new Date(), req.url);
 
-    var headers = JSON.stringify(req.headers, undefined, 4);
-    var now = new Date().toLocaleString();
-    var user_agent = req.headers['user-agent'];
+    weather.getWeather((result) => {
+        res.render('weather', {
+            summary: result.currently.summary + '\n' + result.hourly.summary + '\n' + result.daily.summary
+        })
 
-    res.send(`<h1>It works!</h1>
-    <h3>${now}<h3> 
-    
-    <h3>${user_agent}</h3>
+    })
+});
 
 
-    ${headers}
+// app.get('/', (req, res) => {
+//     console.log(new Date(), req.url);
 
-    <h3><a href='/bit'> SC Price </a></h3>
-    <h3><a href='/weather'> Weather </a></h3>
-    
-    `)
-})
+//     var headers = JSON.stringify(req.headers, undefined, 4);
+//     var now = new Date().toLocaleString();
+//     var user_agent = req.headers['user-agent'];
+
+//     res.send(`<h1>It works!</h1>
+//     <h3>${now}<h3> 
+
+//     <h3>${user_agent}</h3>
+
+
+//     ${headers}
+
+//     <h3><a href='/bit'> SC Price </a></h3>
+//     <h3><a href='/weather'> Weather </a></h3>
+
+//     `)
+// })
 
 
 
 app.get('/bit', (req, res) => {
 
-    coin.getPrice('btc-sc', (data)=>{
+    coin.getPrice('btc-sc', (data) => {
         console.log(JSON.stringify(data, null, 2));
         // res.send(JSON.stringify(data, null, 2));
 
         res.render('bit.hbs', data)
     });
-    
+
     // res.send('coin price');
 })
 
